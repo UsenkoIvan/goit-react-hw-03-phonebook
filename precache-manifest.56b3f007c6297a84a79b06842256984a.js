dd5915aee76ce4ff1daa0a4da7d7666c93961621
Ivan Usenko <ivanja1994@gmail.com>
2020-04-24 17:59:37 +0300

    Updates

---
+++ precache-manifest.56b3f007c6297a84a79b06842256984a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ac5e01a927757c46f0ec114754a803",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "58e523f78b598d0f40eb",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.e2418179.chunk.css"
+  },
+  {
+    "revision": "f481a138efb77a949331",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b2220302.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b2220302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e523f78b598d0f40eb",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.a2db8c02.chunk.js"
+  },
+  {
+    "revision": "29a1186980fb9c7378f6",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.6be664d9.js"
+  }
+]);
